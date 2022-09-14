0edd50ccccda8c778fa094a19fee75cbc4ab4c26
RafaelOrtizHernandez <rortiz@vanguardia.co>
2022-09-13 20:55:25 -0500

    Función Max()

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-let numero = prompt("Ingrese un número decimal", 0.0);
+let num1 = prompt("Ingrese un número 1", 0);
+let num2 = prompt("Ingrese un número 2", 0);
+let num3 = prompt("Ingrese un número 3", 0);
+let num4 = prompt("Ingrese un número 4", 0);
 
-console.log("El valor que devuelve Math.trunc() es ", Math.trunc(numero));
+console.log("El mayor de los números ingresados es ", Math.max(num1, num2, num3, num4));
