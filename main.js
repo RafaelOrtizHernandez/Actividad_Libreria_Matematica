469ad7263132aadbe0d68281db5f992582aa8f65
RafaelOrtizHernandez <rortiz@vanguardia.co>
2022-09-13 20:57:18 -0500

    Función Min()

--- main.js
+++ main.js
@@ -3,4 +3,4 @@ let num2 = prompt("Ingrese un número 2", 0);
 let num3 = prompt("Ingrese un número 3", 0);
 let num4 = prompt("Ingrese un número 4", 0);
 
-console.log("El mayor de los números ingresados es ", Math.max(num1, num2, num3, num4));
+console.log("El menor de los números ingresados es ", Math.min(num1, num2, num3, num4));
