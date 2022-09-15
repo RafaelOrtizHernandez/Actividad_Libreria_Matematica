dc003a46e0d62413429a0d44d5f37e8c77c643e9
RafaelOrtizHernandez <rortiz@vanguardia.co>
2022-09-15 17:25:47 -0500

    Función Floor()

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 
 let numero = prompt("Ingrese un número decimal", 0.0);
 
-console.log("El número redondeado es:", Math.round(numero));
+console.log("El número redondeado es:", Math.floor(numero));
